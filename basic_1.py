de1a8e8738893f6fef1ab13a86f464bae176f4ce
Shravani <chinnamshravani@gmail.com>
2022-11-07 20:49:45 -0500

    hi

---
+++ basic_1.py
@@ -0,0 +1,37 @@
+
+"""name = input ('what is ur name:')
+fav_col = input ('what is ur fav color:')
+print(name + ' likes ' + fav_col) """
+
+user_det = input('enter the weight in pounds: ')
+convert = float(user_det)/ 2
+print(convert)
+
+'''first ='john'
+last ='smith'
+message=first +' [' +last+ '] is a coder'
+msg= f'{first} [{last}] is a coder'
+print(message)
+print(msg)
+print(10%3)'''
+
+'''float price_house = 1000000
+float p=0
+float p1=0
+float pr=0
+float p2=0
+buy_goodcredit = True
+if buy_goodcredit== True:
+    p= (price_house *10) / 100
+    pr = price_house - p
+    print('the amount need to pay is:' + pr)
+else:
+      p1=(price_house*20)/100
+      p2= price_house - p1
+      print('the amount need to pay is:' + p2)
+
+print('the down payment need to pay is:'+((price_house*20)/100))'''
+
+
+
+
